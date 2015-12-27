0f9e3920201605358ff63bdaa1d10658690814f5
johnkaiser <jk@qzforms.com>
2015-12-27 14:01:14 -0800

    Fixes for regex patterns

--- qz_db_update_SV4.sql
+++ qz_db_update_SV4.sql
@@ -9,4 +9,38 @@ DELETE FROM qz.js
   WHERE filename = 'grid.js' 
   OR filename = 'prompt_rule.js';
 
+--
+-- Correct mistakes in SV3
+-- css_edit filename fields in wrong place,
+-- others change \]\] to \[\]
+--
+UPDATE qz.prompt_rule
+SET
+    regex_pattern = '^[^\s\x01-\x1f\x5c\|\!\"\#\$\%\&\(\)\[\]\*\+\,\/\:\;\<\=\>\?\@\/\^\`\{\}\~]{1,63}$',
+    readonly = 'f'
+WHERE     
+    form_name = 'css_edit' AND fieldname = 'filename';
+
+UPDATE qz.prompt_rule
+SET 
+    regex_pattern = '^[^\s\x01-\x1f\x5c\|\!\"\#\$\%\&\(\)\[\]\*\+\,\/\:\;\<\=\>\?\@\/\^\`\{\}\~]{1,63}$',
+WHERE
+    form_name = 'js_edit' AND fieldname = 'filename'
+
+-- for filenames
+UPDATE qz.prompt_rule
+SET regex_pattern = 
+'^[^\s\x01-\x1f\x5c\|\!\"\#\$\%\&\(\)\[\]\*\+\,\-\/\:\;\<\=\>\?\@\/\^\`\{\}\~]{1,63}$'
+WHERE regex_pattern = 
+'^[^\s\x01-\x1f\x5c\|\!\"\#\$\%\&\(\)\]\]\*\+\,\-\/\:\;\<\=\>\?\@\/\^\`\{\}\~]{1,63}$'
+;
+
+--  for variable names
+UPDATE qz.prompt_rule 
+SET regex_pattern = 
+'^[^\s\x01-\x1f\x5c\|\!\"\#\$\%\&\(\)\[\]\*\+\,\-\.\/\:\;\<\=\>\?\@\/\^\`\{\}\~]{1,63}$'
+WHERE regex_pattern = 
+'^[^\s\x01-\x1f\x5c\|\!\"\#\$\%\&\(\)\]\]\*\+\,\-\.\/\:\;\<\=\>\?\@\/\^\`\{\}\~]{1,63}$'
+;
+
 
