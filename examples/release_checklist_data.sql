618c5fd10194a907110f5704e424af6c0a347fa0
johnkaiser <jk@qzforms.com>
2019-04-07 21:35:28 -0700

    Update examples for 0.128

--- examples/release_checklist_data.sql
+++ examples/release_checklist_data.sql
@@ -2,23 +2,29 @@
 -- PostgreSQL database dump
 --
 
+-- Dumped from database version 9.6.5
+-- Dumped by pg_dump version 9.6.5
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
+SET row_security = off;
 
 SET search_path = public, pg_catalog;
 
 --
--- Data for Name: release_checklist; Type: TABLE DATA; Schema: public; Owner: test
+-- Data for Name: release_checklist; Type: TABLE DATA; Schema: public; Owner: jk
 --
 
-INSERT INTO release_checklist (version, seems_to_work, compiles_clean, installs_clean, simple_test_on_install, upgrades_clean, simple_test_on_upgrade, git_up_to_date, qzforms_com_up_to_date) VALUES (0.124, true, true, true, true, true, true, true, true);
-INSERT INTO release_checklist (version, seems_to_work, compiles_clean, installs_clean, simple_test_on_install, upgrades_clean, simple_test_on_upgrade, git_up_to_date, qzforms_com_up_to_date) VALUES (0.123, true, true, true, true, true, true, true, true);
-INSERT INTO release_checklist (version, seems_to_work, compiles_clean, installs_clean, simple_test_on_install, upgrades_clean, simple_test_on_upgrade, git_up_to_date, qzforms_com_up_to_date) VALUES (0.126, true, true, true, true, true, true, false, false);
-INSERT INTO release_checklist (version, seems_to_work, compiles_clean, installs_clean, simple_test_on_install, upgrades_clean, simple_test_on_upgrade, git_up_to_date, qzforms_com_up_to_date) VALUES (0.127, true, true, true, true, true, true, true, true);
+INSERT INTO release_checklist VALUES (0.124, true, true, true, true, true, true, true, true);
+INSERT INTO release_checklist VALUES (0.123, true, true, true, true, true, true, true, true);
+INSERT INTO release_checklist VALUES (0.126, true, true, true, true, true, true, false, false);
+INSERT INTO release_checklist VALUES (0.127, true, true, true, true, true, true, true, true);
+INSERT INTO release_checklist VALUES (0.128, true, true, true, true, true, true, true, true);
 
 
 --
