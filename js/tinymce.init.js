4f2110a296794f0e0f639226212d97866809e499
johnkaiser <jk@qzforms.com>
2020-11-24 14:05:12 -0800

    Allow links to images but not uploads in tinymce

--- js/tinymce.init.js
+++ js/tinymce.init.js
@@ -1,8 +1,9 @@
 tinymce.init(
  {selector: 'textarea.tinymce',
   entity_encoding : "numeric",
-  plugins: [ 'lists table code link' ],
-  toolbar: ['undo redo |styleselect bold italic | bullist numlist blockquote  outdent indent  | link table code'],
+  plugins: [ 'lists table code link image' ],
+  toolbar: ['undo redo |styleselect bold italic | bullist numlist blockquote  outdent indent  | link table code | image'],
   menubar: false,
+  image_uploadtab: false
  });
 
