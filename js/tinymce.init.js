8e04f6a738bc91533a29c4d9d7615ff2cfc37d9b
johnkaiser <jk@qzforms.com>
2020-12-09 15:31:42 -0800

    Silence warnings when tinymce is not used

--- js/tinymce.init.js
+++ js/tinymce.init.js
@@ -1,9 +1,10 @@
-tinymce.init(
- {selector: 'textarea.tinymce',
-  entity_encoding : "numeric",
-  plugins: [ 'lists table code link image' ],
-  toolbar: ['undo redo |styleselect bold italic | bullist numlist blockquote  outdent indent  | link table code | image'],
-  menubar: false,
-  image_uploadtab: false
- });
-
+if (window.hasOwnProperty('tinymce')){
+    tinymce.init(
+     {selector: 'textarea.tinymce',
+      entity_encoding : "numeric",
+      plugins: [ 'lists table code link image' ],
+      toolbar: ['undo redo |styleselect bold italic | bullist numlist blockquote  outdent indent  | link table code | image'],
+      menubar: false,
+      image_uploadtab: false
+     });
+}
